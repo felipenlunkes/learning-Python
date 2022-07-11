a33ae8c5bc68b036c09163c3f77c46c12294fd51
Felipe Lunkes <felipemiguel_nery@hotmail.com>
2022-07-10 23:02:53 -0300

    mkGIF.py

---
+++ mkGIF.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#
+# Created by Felipe Miguel Nery Lunkes (10/07/2022)
+#
+# pip install imageio
+
+# First, the filenames
+
+IMGFile=input('Filename of the images: ')
+Files=IMGFile.split(' ')
+
+for x in range(0, len(Files)):
+    print(Files[x])
+
+# TODO 
